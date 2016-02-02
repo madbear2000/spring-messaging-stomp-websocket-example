87551cd54e0d1fb87e2a72eddf622ea768a37a00
Andrew Vaughan <avaughan@afilias.info>
2016-02-02 17:13:56 +0000

    inital code

---
+++ src/main/java/com/example/HelloMessage.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class HelloMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/example/SpringMessagingStompWebsocketExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingStompWebsocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessagingStompWebsocketExampleApplication.class, args);
+	}
+}
